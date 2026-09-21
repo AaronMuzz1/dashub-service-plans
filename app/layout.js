e1cdc99aa01bd914281950d3aaaefd768adc5526
Aaron <aaron.musgrove@gazley.com>
2026-09-22 11:44:24 +1200

    BUILD-001: add app/layout.js

---
+++ app/layout.js
@@ -0,0 +1,3 @@
+import "./globals.css";
+export const metadata={title:"Dashub Service Plans",description:"Service plan administration"};
+export default function RootLayout({children}){return <html lang="en"><body>{children}</body></html>}
